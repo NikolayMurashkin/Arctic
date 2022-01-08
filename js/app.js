9334cd20c82462dcbf36451ae1726e64fea184ae
Nikolay <82240268+NikolayMurashkin@users.noreply.github.com>
2022-01-08 20:42:45 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,423 @@
+(() => {
+  "use strict";
+  function t(t) {
+    this.type = t;
+  }
+  (t.prototype.init = function () {
+    const t = this;
+    (this.оbjects = []),
+      (this.daClassname = "_dynamic_adapt_"),
+      (this.nodes = document.querySelectorAll("[data-da]"));
+    for (let t = 0; t < this.nodes.length; t++) {
+      const e = this.nodes[t],
+        o = e.dataset.da.trim().split(","),
+        n = {};
+      (n.element = e),
+        (n.parent = e.parentNode),
+        (n.destination = document.querySelector(o[0].trim())),
+        (n.breakpoint = o[1] ? o[1].trim() : "767"),
+        (n.place = o[2] ? o[2].trim() : "last"),
+        (n.index = this.indexInParent(n.parent, n.element)),
+        this.оbjects.push(n);
+    }
+    this.arraySort(this.оbjects),
+      (this.mediaQueries = Array.prototype.map.call(
+        this.оbjects,
+        function (t) {
+          return (
+            "(" + this.type + "-width: " + t.breakpoint + "px)," + t.breakpoint
+          );
+        },
+        this
+      )),
+      (this.mediaQueries = Array.prototype.filter.call(
+        this.mediaQueries,
+        function (t, e, o) {
+          return Array.prototype.indexOf.call(o, t) === e;
+        }
+      ));
+    for (let e = 0; e < this.mediaQueries.length; e++) {
+      const o = this.mediaQueries[e],
+        n = String.prototype.split.call(o, ","),
+        a = window.matchMedia(n[0]),
+        r = n[1],
+        s = Array.prototype.filter.call(this.оbjects, function (t) {
+          return t.breakpoint === r;
+        });
+      a.addListener(function () {
+        t.mediaHandler(a, s);
+      }),
+        this.mediaHandler(a, s);
+    }
+  }),
+    (t.prototype.mediaHandler = function (t, e) {
+      if (t.matches)
+        for (let t = 0; t < e.length; t++) {
+          const o = e[t];
+          (o.index = this.indexInParent(o.parent, o.element)),
+            this.moveTo(o.place, o.element, o.destination);
+        }
+      else
+        for (let t = e.length - 1; t >= 0; t--) {
+          const o = e[t];
+          o.element.classList.contains(this.daClassname) &&
+            this.moveBack(o.parent, o.element, o.index);
+        }
+    }),
+    (t.prototype.moveTo = function (t, e, o) {
+      e.classList.add(this.daClassname),
+        "last" === t || t >= o.children.length
+          ? o.insertAdjacentElement("beforeend", e)
+          : "first" !== t
+          ? o.children[t].insertAdjacentElement("beforebegin", e)
+          : o.insertAdjacentElement("afterbegin", e);
+    }),
+    (t.prototype.moveBack = function (t, e, o) {
+      e.classList.remove(this.daClassname),
+        void 0 !== t.children[o]
+          ? t.children[o].insertAdjacentElement("beforebegin", e)
+          : t.insertAdjacentElement("beforeend", e);
+    }),
+    (t.prototype.indexInParent = function (t, e) {
+      const o = Array.prototype.slice.call(t.children);
+      return Array.prototype.indexOf.call(o, e);
+    }),
+    (t.prototype.arraySort = function (t) {
+      "min" === this.type
+        ? Array.prototype.sort.call(t, function (t, e) {
+            return t.breakpoint === e.breakpoint
+              ? t.place === e.place
+                ? 0
+                : "first" === t.place || "last" === e.place
+                ? -1
+                : "last" === t.place || "first" === e.place
+                ? 1
+                : t.place - e.place
+              : t.breakpoint - e.breakpoint;
+          })
+        : Array.prototype.sort.call(t, function (t, e) {
+            return t.breakpoint === e.breakpoint
+              ? t.place === e.place
+                ? 0
+                : "first" === t.place || "last" === e.place
+                ? 1
+                : "last" === t.place || "first" === e.place
+                ? -1
+                : e.place - t.place
+              : e.breakpoint - t.breakpoint;
+          });
+    });
+  new t("max").init();
+  let e = {
+    Android: function () {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+      return (
+        e.Android() || e.BlackBerry() || e.iOS() || e.Opera() || e.Windows()
+      );
+    },
+  };
+  let o = !0,
+    n = (t = 500) => {
+      let e = document.querySelector("body");
+      if (o) {
+        let n = document.querySelectorAll("[data-lp]");
+        setTimeout(() => {
+          for (let t = 0; t < n.length; t++) {
+            n[t].style.paddingRight = "0px";
+          }
+          (e.style.paddingRight = "0px"),
+            document.documentElement.classList.remove("lock");
+        }, t),
+          (o = !1),
+          setTimeout(function () {
+            o = !0;
+          }, t);
+      }
+    },
+    a = (t = 500) => {
+      let e = document.querySelector("body");
+      if (o) {
+        let n = document.querySelectorAll("[data-lp]");
+        for (let t = 0; t < n.length; t++) {
+          n[t].style.paddingRight =
+            window.innerWidth -
+            document.querySelector(".wrapper").offsetWidth +
+            "px";
+        }
+        (e.style.paddingRight =
+          window.innerWidth -
+          document.querySelector(".wrapper").offsetWidth +
+          "px"),
+          document.documentElement.classList.add("lock"),
+          (o = !1),
+          setTimeout(function () {
+            o = !0;
+          }, t);
+      }
+    };
+  function r(t) {
+    setTimeout(() => {
+      window.FLS && console.log(t);
+    }, 0);
+  }
+  function s(t) {
+    return t.filter(function (t, e, o) {
+      return o.indexOf(t) === e;
+    });
+  }
+  let i = (t, e = !1, o = 500, a = 0) => {
+    const s = document.querySelector(t);
+    if (s) {
+      let i = "",
+        c = 0;
+      e &&
+        ((i = "header.header"), (c = document.querySelector(i).offsetHeight));
+      let l = {
+        speedAsDuration: !0,
+        speed: o,
+        header: i,
+        offset: a,
+        easing: "easeOutQuad",
+      };
+      if (
+        (document.documentElement.classList.contains("menu-open") &&
+          (n(), document.documentElement.classList.remove("menu-open")),
+        "undefined" != typeof SmoothScroll)
+      )
+        new SmoothScroll().animateScroll(s, "", l);
+      else {
+        let t = s.getBoundingClientRect().top + scrollY;
+        window.scrollTo({ top: c ? t - c : t, behavior: "smooth" });
+      }
+      r(`[gotoBlock]: Юхуу...едем к ${t}`);
+    } else r(`[gotoBlock]: Ой ой..Такого блока нет на странице: ${t}`);
+  };
+  class c {
+    constructor(t) {
+      (this.config = Object.assign({ logging: !0 }, t)),
+        this.observer,
+        !document.documentElement.classList.contains("watcher") &&
+          this.scrollWatcherRun();
+    }
+    scrollWatcherUpdate() {
+      this.scrollWatcherRun();
+    }
+    scrollWatcherRun() {
+      document.documentElement.classList.add("watcher"),
+        this.scrollWatcherConstructor(
+          document.querySelectorAll("[data-watch]")
+        );
+    }
+    scrollWatcherConstructor(t) {
+      if (t.length) {
+        this.scrollWatcherLogging(
+          `Проснулся, слежу за объектами (${t.length})...`
+        ),
+          s(
+            Array.from(t).map(function (t) {
+              return `${
+                t.dataset.watchRoot ? t.dataset.watchRoot : null
+              }|${t.dataset.watchMargin ? t.dataset.watchMargin : "0px"}|${t.dataset.watchThreshold ? t.dataset.watchThreshold : 0}`;
+            })
+          ).forEach((e) => {
+            let o = e.split("|"),
+              n = { root: o[0], margin: o[1], threshold: o[2] },
+              a = Array.from(t).filter(function (t) {
+                let e = t.dataset.watchRoot ? t.dataset.watchRoot : null,
+                  o = t.dataset.watchMargin ? t.dataset.watchMargin : "0px",
+                  a = t.dataset.watchThreshold ? t.dataset.watchThreshold : 0;
+                if (
+                  String(e) === n.root &&
+                  String(o) === n.margin &&
+                  String(a) === n.threshold
+                )
+                  return t;
+              }),
+              r = this.getScrollWatcherConfig(n);
+            this.scrollWatcherInit(a, r);
+          });
+      } else
+        this.scrollWatcherLogging("Сплю, нет объектов для слежения. ZzzZZzz");
+    }
+    getScrollWatcherConfig(t) {
+      let e = {};
+      if (
+        (document.querySelector(t.root)
+          ? (e.root = document.querySelector(t.root))
+          : "null" !== t.root &&
+            this.scrollWatcherLogging(
+              `Эмм... родительского объекта ${t.root} нет на странице`
+            ),
+        (e.rootMargin = t.margin),
+        !(t.margin.indexOf("px") < 0 && t.margin.indexOf("%") < 0))
+      ) {
+        if ("prx" === t.threshold) {
+          t.threshold = [];
+          for (let e = 0; e <= 1; e += 0.005) t.threshold.push(e);
+        } else t.threshold = t.threshold.split(",");
+        return (e.threshold = t.threshold), e;
+      }
+      this.scrollWatcherLogging(
+        "Ой ой, настройку data-watch-margin нужно задавать в PX или %"
+      );
+    }
+    scrollWatcherCreate(t) {
+      this.observer = new IntersectionObserver((t, e) => {
+        t.forEach((t) => {
+          this.scrollWatcherCallback(t, e);
+        });
+      }, t);
+    }
+    scrollWatcherInit(t, e) {
+      this.scrollWatcherCreate(e), t.forEach((t) => this.observer.observe(t));
+    }
+    scrollWatcherIntersecting(t, e) {
+      t.isIntersecting
+        ? (!e.classList.contains("_watcher-view") &&
+            e.classList.add("_watcher-view"),
+          this.scrollWatcherLogging(
+            `Я вижу ${e.classList}, добавил класс _watcher-view`
+          ))
+        : (e.classList.contains("_watcher-view") &&
+            e.classList.remove("_watcher-view"),
+          this.scrollWatcherLogging(
+            `Я не вижу ${e.classList}, убрал класс _watcher-view`
+          ));
+    }
+    scrollWatcherOff(t, e) {
+      e.unobserve(t),
+        this.scrollWatcherLogging(`Я перестал следить за ${t.classList}`);
+    }
+    scrollWatcherLogging(t) {
+      this.config.logging && r(`[Наблюдатель]: ${t}`);
+    }
+    scrollWatcherCallback(t, e) {
+      const o = t.target;
+      this.scrollWatcherIntersecting(t, o),
+        o.hasAttribute("data-watch-once") &&
+          t.isIntersecting &&
+          this.scrollWatcherOff(o, e),
+        document.dispatchEvent(
+          new CustomEvent("watcherCallback", { detail: { entry: t } })
+        );
+    }
+  }
+  let l = !1;
+  setTimeout(() => {
+    if (l) {
+      let t = new Event("windowScroll");
+      window.addEventListener("scroll", function (e) {
+        document.dispatchEvent(t);
+      });
+    }
+  }, 0),
+    (window.FLS = !0),
+    (function (t) {
+      let e = new Image();
+      (e.onload = e.onerror =
+        function () {
+          t(2 == e.height);
+        }),
+        (e.src =
+          "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA");
+    })(function (t) {
+      let e = !0 === t ? "webp" : "no-webp";
+      document.documentElement.classList.add(e);
+    }),
+    e.any() && document.documentElement.classList.add("touch"),
+    (function () {
+      let t = document.querySelector(".icon-menu");
+      t &&
+        t.addEventListener("click", function (t) {
+          o &&
+            (((t = 500) => {
+              document.documentElement.classList.contains("lock") ? n(t) : a(t);
+            })(),
+            document.documentElement.classList.toggle("menu-open"));
+        });
+    })(),
+    (function () {
+      if (document.querySelectorAll("[data-fullscreen]").length && e.any()) {
+        function t() {
+          let t = 0.01 * window.innerHeight;
+          document.documentElement.style.setProperty("--vh", `${t}px`);
+        }
+        window.addEventListener("resize", t), t();
+      }
+    })(),
+    new c({}),
+    (function () {
+      function t(t) {
+        if ("click" === t.type) {
+          const e = t.target;
+          if (e.closest("[data-goto]")) {
+            const o = e.closest("[data-goto]"),
+              n = o.dataset.goto ? o.dataset.goto : "",
+              a = !!o.hasAttribute("data-goto-header"),
+              r = o.dataset.gotoSpeed ? o.dataset.gotoSpeed : "500";
+            i(n, a, r), t.preventDefault();
+          }
+        } else if ("watcherCallback" === t.type && t.detail) {
+          const e = t.detail.entry,
+            o = e.target;
+          if ("navigator" === o.dataset.watch) {
+            const t = o.id,
+              n =
+                (document.querySelector("[data-goto]._navigator-active"),
+                document.querySelector(`[data-goto="${t}"]`));
+            e.isIntersecting
+              ? n && n.classList.add("_navigator-active")
+              : n && n.classList.remove("_navigator-active");
+          }
+        }
+      }
+      document.addEventListener("click", t),
+        document.addEventListener("watcherCallback", t);
+    })(),
+    (function () {
+      l = !0;
+      const t = document.querySelector("header.header"),
+        e = t.hasAttribute("data-scroll-show"),
+        o = t.dataset.scrollShow ? t.dataset.scrollShow : 500,
+        n = t.dataset.scroll ? t.dataset.scroll : 1;
+      let a,
+        r = 0;
+      document.addEventListener("windowScroll", function (s) {
+        const i = window.scrollY;
+        clearTimeout(a),
+          i >= n
+            ? (!t.classList.contains("_header-scroll") &&
+                t.classList.add("_header-scroll"),
+              e &&
+                (i > r
+                  ? t.classList.contains("_header-show") &&
+                    t.classList.remove("_header-show")
+                  : !t.classList.contains("_header-show") &&
+                    t.classList.add("_header-show"),
+                (a = setTimeout(() => {
+                  !t.classList.contains("_header-show") &&
+                    t.classList.add("_header-show");
+                }, o))))
+            : (t.classList.contains("_header-scroll") &&
+                t.classList.remove("_header-scroll"),
+              e &&
+                t.classList.contains("_header-show") &&
+                t.classList.remove("_header-show")),
+          (r = i <= 0 ? 0 : i);
+      });
+    })();
+})();
